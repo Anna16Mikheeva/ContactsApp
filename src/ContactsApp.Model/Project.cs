b8bf23675b40af824192f4d062950f1d3ff01777
WINDOWS 10 <anna.mav31@gmail.com>
2022-06-15 19:58:52 +0700

    fix(Projectc.cs): исправление ошибки с неправильным поиском именниников

--- src/ContactsApp.Model/Project.cs
+++ src/ContactsApp.Model/Project.cs
@@ -18,5 +18,24 @@ namespace ContactsApp.Model
         /// Возвращает или задает контакт.
         /// </summary>
         public List<Contact> Contacts { get; set; } = new List<Contact>();
+
+        /// <summary>
+        /// Сортировка контакта по алфавиту.
+        /// </summary>
+        public List<Contact> SortContacts(List<Contact> contacts)
+        {
+            contacts = contacts.OrderBy(contact => contact.Surname).ToList();
+            return contacts;
+        }
+
+
+        /// <summary>
+        /// Поиск контакта по дате рождения.
+        /// </summary>
+        public List<Contact> SearchByDateOfirth(List<Contact> contacts)
+        {
+            contacts = contacts.Where(contact => contact.DateOfBirth.Date == DateTime.Now.Date).ToList();
+            return contacts;
+        }
     }
 }
--- src/ContactsApp.Model/ProjectSerializer.cs
+++ src/ContactsApp.Model/ProjectSerializer.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Environment;
 
 namespace ContactsApp.Model
 {
     public class ProjectSerializer
     {
+        public string Filename{ get; set; } = 
+            Environment.GetFolderPath(SpecialFolder.ApplicationData) + "\\Mikheeva\\ContsctsApp";
 
+        public void SaveToFile(Project project)
+        {
+
+        }
+        public Project LoadFromFile()
+        {
+            Project p = new Project();
+            return p;
+        }
+
+        public ProjectSerializer()
+        {
+            
+        }
     }
 }
